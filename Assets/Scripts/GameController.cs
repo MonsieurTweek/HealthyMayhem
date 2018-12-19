7d9d445eaa76117023bcd2bd7ef0c9d89973afe5
Gaëtan Regent <gaetan.regent@ubisoft.com>
2018-12-19 09:43:45 +0100

    Review des UI texts controllers

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -13,25 +13,19 @@ public class GameController : MonoBehaviour
     public GameObject m_Order_Recipe;
     public GameObject m_Order_Customer;
 
-    // Canvas - Ingredients
-    [Header("----- Canvas - Ingredients -----")]
-    public Text m_Apple_Count_Text;
-    public Text m_Banana_Count_Text;
-    public Text m_Carrot_Count_Text;
-    public Text m_Orange_Count_Text;
-    public Text m_Shaker_Count_Text;
-    public Text m_Ingredients_Bell_Count_Text;
-
-    // Canvas - Topping
-    [Header("----- Canvas - Topping -----")]
-    public Text m_Quinoa_Count_Text;
-    public Text m_Goji_Count_Text;
-    public Text m_Mint_Count_Text;
-    public Text m_Topping_Bell_Count_Text;
+    // Texts Controllers
+    [Header("----- Texts Controllers -----")]
+    public List<UITextController> m_TextsUI;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Init UI texts
+        foreach(UITextController textController in m_TextsUI)
+        {
+            textController.ResetCounterToDefault();
+        }
+
         GenerateOrder();
     }
 
@@ -55,69 +49,13 @@ public class GameController : MonoBehaviour
 
     }
 
-    public void SetInputCounterValue(string key, int current, int max)
+    public void SetInputCounterValue(string key, int current, int target)
     {
-
-        Text textToUpdate = null;
-
-        switch (key)
+        foreach (UITextController textController in m_TextsUI)
         {
-            // Ingredients
-            case "apple":
-                textToUpdate = m_Apple_Count_Text;
-                break;
-            case "banana":
-                textToUpdate = m_Banana_Count_Text;
-                break;
-            case "carrot":
-                textToUpdate = m_Carrot_Count_Text;
-                break;
-            case "orange":
-                textToUpdate =  m_Orange_Count_Text;
-                break;
-            case "shaker":
-                textToUpdate = m_Shaker_Count_Text;
-                break;
-            case "ingredient_bell":
-                textToUpdate = m_Ingredients_Bell_Count_Text;
-                break;
-
-            // Topping
-            case "quinoa":
-                textToUpdate = m_Quinoa_Count_Text;
-                break;
-            case "goji":
-                textToUpdate = m_Goji_Count_Text;
-                break;
-            case "mint":
-                textToUpdate = m_Mint_Count_Text;
-                break;
-            case "topping_bell":
-                textToUpdate = m_Topping_Bell_Count_Text;
-                break;
-
-            default:
-                break;
-        }
-
-        if(textToUpdate != null)
-        {
-            textToUpdate.text = current + "/" + max;
-            if(max == 0)
+            if(textController.m_QualifiedName == key)
             {
-                textToUpdate.color = new Color(0.65f, 0.65f, 0.65f);
-            } else
-            {
-                if(current < max)
-                {
-                    textToUpdate.color = new Color(0.12f, 0.12f, 0.12f);
-                } else if(current == max)
-                {
-                    textToUpdate.color = new Color(0f, 0.69f, 0.31f);
-                } else
-                {
-                    textToUpdate.color = new Color(1f, 0.28f, 0.28f);
-                }
+                textController.SetCounter(current, target);
             }
         }
     }
---
+++ Assets/Scripts/UITextController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextController : MonoBehaviour
+{
+
+    public string m_QualifiedName;
+
+    private Text m_TextComponent;
+    private Color m_Green = new Color(0f, 0.69f, 0.31f);
+    private Color m_Red = new Color(1f, 0.28f, 0.28f);
+    private Color m_Black = new Color(0.12f, 0.12f, 0.12f);
+    private Color m_Grey = new Color(0.65f, 0.65f, 0.65f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_TextComponent = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetCounterToDefault()
+    {
+        m_TextComponent.text = "-/-";
+        m_TextComponent.color = m_Grey;
+    }
+
+    public void SetCounter(int current, int target)
+    {
+        m_TextComponent.text = current + "/" + target;
+        if(current < target)
+        {
+            m_TextComponent.color = m_Black;
+        } else if(current > target)
+        {
+            m_TextComponent.color = m_Red;
+        } else
+        {
+            m_TextComponent.color = m_Green;
+        }
+        
+    }
+
+}
